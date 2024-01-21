b2c1150d096e69c1021cb2749c4f6004f2dff8dd
JGrant024 <jonathan.grant23@icloud.com>
2024-01-20 20:27:44 -0500

    added game file and class

---
+++ warrior_game.py
@@ -0,0 +1,11 @@
+import random
+
+class Character:
+    def __init__(self, health, power, name, coins=20):
+        self.health = health
+        self.power = power
+        self.name = name
+        self.coins = coins
+
+
+    
