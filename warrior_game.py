a918cb5f408c4e3dd9dac7603e1329ac5a5dd51a
JGrant024 <jonathan.grant23@icloud.com>
2024-01-21 23:34:04 -0500

    fixed bug in shopping input to allow user to shop

--- warrior_game.py
+++ warrior_game.py
@@ -9,7 +9,7 @@ class Character:
 
 
     def alive(self): 
-        return self.health > 0
+        return self.health > 1
     
     def attack(self, enemy):   
         damage = random.randint(1, self.power)
@@ -23,10 +23,10 @@ class Character:
     def print_status(self, enemy):
         print(f"{self.name}: {self.health} | {enemy.name}: {enemy.health} | Coins: {self.coins}")
 
-    def buy(self, item): 
+    def buy(self, item,): 
         if self.coins >= item.cost: 
             self.coins -= item.cost
-            item.apply(self)
+            item.apply_item(self)
             print(f"{self.name} has purchased {item.name} for {item.cost} coins!")
         else: 
             print(f"Oops! {self.name} does not have enough for {self.Item}")
@@ -45,23 +45,25 @@ class Enemy(Character):
         self.bounty = bounty 
 
 #created a class for the charater with default value for health, power, and bounty. 
-class Shadow(Enemy): 
-    def __init__(self): 
-        super().__init__(health =1, power = 1, name ="Shadow", bounty = 6)
+class Shadow(Enemy):  
+        def __init__(self): 
+            super().__init__(health=1, power=1, name="Shadow", bounty=6)
 
-    # This allow the hero to take damage once out of every ten times hit. 
-    def attack(self, hero): 
-        if random.randint(1, 10) == 1:
-            print(f"{self.name} is quicker than that! You almost had it!")
-        else: 
-            super().attack(hero)
+    # This allows the hero to take damage once out of every ten times hit. 
+        def attack(self, hero): 
+            if random.randint(1, 10) == 1:
+                print(f"{self.name} is quicker than that! You almost had it!")
+            else: 
+                super().attack(hero)
+
+     
 
 #Make a Zombie character that doesn't die even if their health is below zero.
 class Zombie(Enemy): 
     def __init__(self): 
         super().__init__(health =5, power =2, name="Zombie", bounty=8  )
     # This method makes sure that the zombie stays alive 
-    def alive(): 
+    def alive(self): 
         return True 
     
 class Wizard(Enemy): 
@@ -83,6 +85,11 @@ class Store():
     ]
 
     def go_shopping(self, hero): 
+        
+
+
+
+
         ascii_art = """
 
 
@@ -96,22 +103,20 @@ class Store():
 
  """
         print(ascii_art)
-                     
         print(f"Welcome to Warrior Outlets! Bank Balance: {hero.coins}")
         print("Check out our avaliable items:!")
         #for loop for items taken from inventory
         for items in self.Items: 
             print(f"{items['name']} - {items['cost']} coins.")
 
-        selction = input("Enter the name of your selected item, or type 'exit' to leave:  ")
+        selection = input("Enter the name of your selected item, or type 'exit' to leave:  ")
         
-        if selction.lower() == 'exit': 
+        if selection.lower() == 'exit': 
             print("Thanks!! Come back soon!")
         else: 
-            item = next(( i for i in self.Items if i ['name'].lower == selction.lower()), None)
+            item = next((i for i in self.Items if i['name'].lower() == selection.lower()), None)
             if item: 
-                # globals() function is a built-in function that returns a dictionary representing the current global symbol table.  https://www.geeksforgeeks.org/python-globals-function/
-                hero.buy(globals()[item["class"]]())
+                hero.buy(globals()[item["class"]](name = item["name"], cost = item["cost"]))
                 self.go_shopping(hero)
             else: 
                 print("Oops! Pick something valid from the selection!")
@@ -130,8 +135,133 @@ class Item:
 class Tonic(Item): 
     def __init__(self, name, cost):
         super().__init__(name="Tonic", cost=5)
-
-
-
-
     
+    def apply_item(self, character):
+        character.health += 2
+
+class Sword(Item): 
+    def __init__(self, name, cost):
+        super().__init__(name="Sword", cost=8)
+    
+    def apply_item(self, character):
+        character.power += 2
+
+
+
+hero = Hero(name = "Hero", health = 10, power = 5)
+shadow = Shadow() 
+zombie = Zombie() 
+wizard = Wizard()
+archer = Archer() 
+store = Store()     
+
+
+ascii_art = """
+
+
+ _______                        __             _            _______                          __       __         _____    
+|_   __ \                      |  ]           / |_         |_   __ \                        [  |     [  |       / ___ `.  
+  | |__) |  .---.  ,--.    .--.| |   _   __  `| |-' .--.     | |__) |  __   _   _ .--..--.   | |.--.  | | .---.|_/___) |  
+  |  __ /  / /__\\`'_\ : / /'`\' |  [ \ [  ]  | | / .'`\ \   |  __ /  [  | | | [ `.-. .-. |  | '/'`\ \| |/ /__\\ /  __.'  
+ _| |  \ \_| \__.,// | |,| \__/  |   \ '/ /   | |,| \__. |  _| |  \ \_ | \_/ |, | | | | | |  |  \__/ || || \__., |_|      
+|____| |___|'.__.'\'-;__/ '.__.;__][\_:  /    \__/ '.__.'  |____| |___|'.__.'_/[___||__||__][__;.__.'[___]'.__.' (_)      
+                                    \__.'                                                                                 
+
+                                                                                                           
+
+"""
+
+
+
+
+while hero.alive():
+    print(ascii_art)
+    print("Choose one of the following options or get ready for a rumble!")
+    print("1. Shadow")
+    print("2. Zombie")
+    print("3. Wizard")
+    print("4. Archer")
+    print("5. Go Shopping")
+    print("6. Quit")
+
+    user_input = input()
+
+    if user_input == "1":
+        enemy = shadow
+    elif user_input == "2":
+        enemy = zombie
+    elif user_input == "3":
+        enemy = wizard
+    elif user_input == "4":
+        enemy = archer
+    elif user_input == "5":
+        store.go_shopping(hero)
+        continue
+    elif user_input == "6":
+        print("See ya L8r!")
+        break
+    else:
+        print("Opps! Choose valid option!")
+        continue
+
+while hero.alive():
+    print(ascii_art)
+    print("Choose one of the following options or get ready for a rumble!")
+    print("1. Shadow")
+    print("2. Zombie")
+    print("3. Wizard")
+    print("4. Archer")
+    print("5. Go Shopping")
+    print("6. Quit")
+
+    user_input = input()
+
+    if user_input == "1":
+        enemy = shadow
+    elif user_input == "2":
+        enemy = zombie
+    elif user_input == "3":
+        enemy = wizard
+    elif user_input == "4":
+        enemy = archer
+    elif user_input == "5":
+        Store.go_shopping(hero)
+        continue
+    elif user_input == "6":
+        print("See ya L8r!")
+        break
+    else:
+        print("Opps! Choose valid option!")
+        continue
+
+    while enemy.alive() and hero.alive():
+        print("What do you want to do?")
+        print("1. Rumble")
+        print("2. Do nothing")
+        print("3. Get out there!")
+        user_input = input()
+
+        if user_input == "1":
+            hero.attack(enemy)
+            enemy.attack(hero)
+            hero.print_status(enemy)
+
+            if not enemy.alive():
+                print(f"You taught {enemy.name} a lesson, and earned {enemy.bounty} coins!")
+                hero.coins += enemy.bounty
+                break
+
+        elif user_input == "2":
+            enemy.attack(hero)
+            hero.print_status(enemy)
+
+        elif user_input == "3":
+            print("The Battle was too much! You left!")
+            break
+
+        else:
+            print("Opps! Choose a valid option!")
+
+        if not hero.alive():
+            print("You have been unalived. Try again? ")
+            break
